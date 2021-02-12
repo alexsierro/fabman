8d3ff29e4f2745e86cb6b0f1af672e55ce61049b
EdwinLocker <lockeredwin@gmail.com>
2021-02-12 18:02:32 +0100

    add tarif choice field

--- members/forms.py
+++ members/forms.py
@@ -4,6 +4,16 @@ from .models import Member
 
 
 class InscriptionForm(forms.ModelForm):
+
+    choice_inscription = [
+        ('Tarif réduit', 'Tarif réduit'),
+        ('Tarif plein', 'Tarif plein'),
+        ('Tarif pro', 'Tarif pro'),
+        ('Tarif mécène', 'Tarif mécène')
+    ]
+
     class Meta:
         model = Member
         fields = ('civility', 'name', 'surname', 'address', 'locality', 'npa', 'mail', 'phone_number')
+
+    Choix_tarif = forms.ChoiceField(choices=choice_inscription)
