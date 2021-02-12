0d7fcb96e18948b0e84665652e57e66491857005
EdwinLocker <lockeredwin@gmail.com>
2021-02-12 18:26:30 +0100

    minor changes on views of invoicing

--- invoicing/views.py
+++ invoicing/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404, render
 from invoicing.models import Invoice, Usage, AccountEntry
 from django.db.models import Max, Sum
 
-# Create your views here.
 from members.models import Member
 
 
@@ -23,31 +22,40 @@ def preview(request):
 
         total_amount = usages.aggregate(total=Sum('total_price'))['total']
 
-
-
-        usages_annotated = usages.values('resource__name', 'resource__unit__name', 'unit_price', 'project__name').annotate(qty=Sum('qty'),
-                                                                                                   total_price=Sum(
-                                                                                                       'total_price')).order_by(
-            'project__name')
+        usages_annotated = usages.values('resource__name',
+                                         'resource__unit__name',
+                                         'unit_price',
+                                         'project__name').annotate(qty=Sum('qty'), total_price=Sum('total_price')
+                                                                   ).order_by('project__name')
 
         # information about machine hours for animators
         amount_machine_before = AccountEntry.objects.aggregate(total=Sum('amount_machine'))['total']
-        amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(total=Sum('total_price'))['total']
+        amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(
+            total=Sum('total_price'))['total']
         deduction = min(amount_machine_before, amount_machine_usages)
         amount_machine_after = amount_machine_before - deduction
 
         amount_due = total_amount - deduction
 
-        invoice = Invoice(amount=total_amount, amount_deduction=deduction, amount_due=amount_due, member=member, invoice_number=invoice_number)
+        invoice = Invoice(amount=total_amount,
+                          amount_deduction=deduction,
+                          amount_due=amount_due,
+                          member=member,
+                          invoice_number=invoice_number)
 
         print(total_amount)
 
         return render(request, 'invoice.html',
-                      {'usages': usages, 'usages_anotated': usages_annotated, 'member_info': member,
-                       'choice_member': choice_member, 'invoice': invoice, 'amount_machine_before': amount_machine_before,
-                       'amount_machine_after': amount_machine_after, 'amount_machine_usages': amount_machine_usages,
-
-                       })
+                      {'usages': usages,
+                       'usages_anotated': usages_annotated,
+                       'member_info': member,
+                       'choice_member': choice_member,
+                       'invoice': invoice,
+                       'amount_machine_before': amount_machine_before,
+                       'amount_machine_after': amount_machine_after,
+                       'amount_machine_usages': amount_machine_usages
+                       }
+                      )
 
 
 def create(request):
@@ -66,13 +74,14 @@ def create(request):
         total_amount = usages.aggregate(total=Sum('total_price'))['total']
 
         invoice = Invoice(amount=total_amount, member=member, invoice_number=invoice_number)
-
         invoice.save()
 
         usages.update(invoice=invoice)
-
         usages = Usage.objects.filter(invoice=invoice)
 
         return render(request, 'invoice.html',
-                      {'usages': usages, 'member_info': member,
-                       'choice_member': choice_member, 'invoice': invoice})
+                      {'usages': usages,
+                       'member_info': member,
+                       'choice_member': choice_member,
+                       'invoice': invoice}
+                      )
