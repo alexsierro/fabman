37fcf472d6a874e9b4d94986746b8561a3103f1a
Alexandre Sierro <alexsierro@gmail.com>
2021-03-26 22:14:07 +0100

    Add cash balance deduction

--- invoicing/views.py
+++ invoicing/views.py
@@ -5,62 +5,72 @@ from django.db.models import Max, Sum
 from qrbill.bill import QRBill
 from stdnum.ch import esr
 
-
 from members.models import Member
 
-def prepare(request, create = False):
 
-        member_id = request.POST['member_id']
+def prepare(request, create=False):
+    member_id = request.POST['member_id']
 
-        invoice_number_max = Invoice.objects.all().aggregate(Max('invoice_number'))['invoice_number__max']
-        invoice_number = invoice_number_max + 1
+    invoice_number_max = Invoice.objects.all().aggregate(Max('invoice_number'))['invoice_number__max']
+    invoice_number = invoice_number_max + 1
 
-        member = Member.objects.get(pk=member_id)
-        usages = Usage.objects.filter(member=member, valid=True, invoice=None)
+    member = Member.objects.get(pk=member_id)
+    usages = Usage.objects.filter(member=member, valid=True, invoice=None)
 
-        total_amount = usages.aggregate(total=Sum('total_price'))['total'] or 0
+    total_amount = usages.aggregate(total=Sum('total_price'))['total'] or 0
 
-        usages_annotated = usages.values('resource__name',
-                                         'resource__unit__name',
-                                         'unit_price',
-                                         'project__name').annotate(qty=Sum('qty'), total_price=Sum('total_price')
-                                                                   ).order_by('project__name')
+    usages_annotated = usages.values('resource__name',
+                                     'resource__unit__name',
+                                     'unit_price',
+                                     'project__name').annotate(qty=Sum('qty'), total_price=Sum('total_price')
+                                                               ).order_by('project__name')
 
-        # information about machine hours for animators
-        amount_machine_before = AccountEntry.objects.filter(member=member).aggregate(total=Sum('amount_machine'))['total'] or 0
-        amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(
-            total=Sum('total_price'))['total'] or 0
+    balance = AccountEntry.objects.filter(member=member).aggregate(machine=Sum('amount_machine'),
+                                                                   cash=Sum('amount_cash'))
 
+    print(balance)
+    # information about machine hours balance
+    amount_machine_before = balance['machine'] or 0
+    amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(
+        total=Sum('total_price'))['total'] or 0
 
+    deduction_machine = min(amount_machine_before, amount_machine_usages)
+    amount_machine_after = amount_machine_before - deduction_machine
 
-        deduction = min(amount_machine_before, amount_machine_usages)
+    # information about cash balance
+    cash_before = balance['cash'] or 0
+    remaining_amount = total_amount - deduction_machine
+    deduction_cash = min(cash_before, remaining_amount)
+    cash_after = cash_before - deduction_cash
 
-        amount_machine_after = amount_machine_before - deduction
+    deduction = deduction_machine + deduction_cash
 
+    invoice = Invoice(amount=total_amount,
+                      amount_deduction=deduction,
+                      member=member,
+                      invoice_number=invoice_number)
 
-        invoice = Invoice(amount=total_amount,
-                          amount_deduction=deduction,
-                          member=member,
-                          invoice_number=invoice_number)
+    print(total_amount)
+    if create:
+        invoice.save()
+        if deduction > 0:
+            AccountEntry.objects.create(member=member, amount_machine=-deduction_machine, amount_cash=-deduction_cash, invoice=invoice)
 
-        print(total_amount)
-        if create:
-            invoice.save()
-            if deduction > 0:
-                AccountEntry.objects.create(member=member, amount_machine=-deduction, invoice=invoice)
-                
-            usages.update(invoice=invoice)
-            usages = Usage.objects.filter(invoice=invoice)
+        usages.update(invoice=invoice)
+        usages = Usage.objects.filter(invoice=invoice)
 
-
-        return {'usages': usages,
-                'usages_anotated': usages_annotated,
-                'member_info': member,
-                'invoice': invoice,
-                'amount_machine_before': amount_machine_before,
-                'amount_machine_after': amount_machine_after,
-                'amount_machine_usages': amount_machine_usages
-                }
+    return {'usages': usages,
+            'usages_anotated': usages_annotated,
+            'member_info': member,
+            'invoice': invoice,
+            'amount_machine_before': amount_machine_before,
+            'amount_machine_after': amount_machine_after,
+            'amount_machine_usages': amount_machine_usages,
+            'deduction_machine': deduction_machine,
+            'cash_before': cash_before,
+            'cash_after': cash_after,
+            'deduction_cash': deduction_cash
+            }
 
 
 def preview(request):
@@ -90,22 +100,23 @@ def create(request):
         result['choice_member'] = choice_member
         return render(request, 'invoice.html', result)
 
-def show(request, invoice_number):
 
-    invoce = Invoice.objects.get(invoice_number = invoice_number)
+def show(request, invoice_number):
+    invoce = Invoice.objects.get(invoice_number=invoice_number)
     number = invoice_number
     number_ref = number + esr.calc_check_digit(number)
 
     my_bill = QRBill(
-         ref_number= number_ref,
-         account='CH5530808007723788063',
-         creditor={
-             'name': 'FabLab Sion' , 'pcode': '1950', 'city': 'Sion',
-         },
-         debtor={
-            'name': invoce.member.name , 'pcode': '1950', 'city': invoce.member.locality, 'street': invoce.member.address,
-         },
-         amount= invoce.amount_due,
+        ref_number=number_ref,
+        account='CH5530808007723788063',
+        creditor={
+            'name': 'FabLab Sion', 'pcode': '1950', 'city': 'Sion',
+        },
+        debtor={
+            'name': invoce.member.name, 'pcode': '1950', 'city': invoce.member.locality,
+            'street': invoce.member.address,
+        },
+        amount=invoce.amount_due,
     )
     my_bill.as_svg('my_bill.svg')
 
