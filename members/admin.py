e96f9d1e1d0849b45f7823185d1280b2d82260d0
Alexandre Sierro <alexsierro@gmail.com>
2021-05-01 21:12:18 +0200

    Add colored button for Invoice list in Member admin

--- members/admin.py
+++ members/admin.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils.html import format_html
 
 from .models import Member, Project
+from invoicing.models import Invoice
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -17,9 +18,17 @@ admin.site.register(Project, ProjectAdmin)
 class MemberAdmin(admin.ModelAdmin):
 
     def members_actions(self, obj):
-        return format_html('<a class="button" href="{}" target="_blank">Show</a>', reverse('show_members', args=[obj.pk]))
+        open_invoices = Invoice.objects.filter(member=obj).exclude(status__in=('paid', 'cancelled')).count()
+        color = 'darkgreen'
+        if open_invoices > 0:
+            color = 'darkorange'
+        if open_invoices > 1:
+            color = 'red'
+        return format_html('<a class="button" href="{}" style="background-color:{}"target="_blank">Invoices ({})</a>', reverse('show_members', args=[obj.pk]), color, open_invoices)
     list_display = ['members_actions', 'name', 'surname', 'rfid', 'is_member', 'is_staff']
+    list_display_links = ['name', 'surname']
     search_fields = ['name', 'surname', 'rfid']
+    ordering = ['name', 'surname']
 
 
 admin.site.register(Member, MemberAdmin)
