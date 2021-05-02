91acfa6417466626f6ab2847ed2f48820b70cafe
Alexandre Sierro <alexsierro@gmail.com>
2021-05-02 19:53:16 +0200

    Add member type and is resigned

--- members/models.py
+++ members/models.py
@@ -9,6 +9,14 @@ class Member(models.Model):
         ('member', 'member')
     ]
 
+    MEMBER_TYPE = [
+        ('membre', 'membre'),
+        ('etudiant', 'étudiant'),
+        ('passif', 'passif'),
+        ('angel', 'angel'),
+        ('alias', 'alias de facturation'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.PROTECT, default=None, null=True, blank=True)
     name = models.CharField('Nom', max_length=200)
     surname = models.CharField('Prénom', max_length=200)
@@ -20,11 +28,13 @@ class Member(models.Model):
     mail = models.EmailField('Email', max_length=200, default=None, null=True, blank=True)
     phone_number = models.CharField('Téléphone', max_length=25, default=None, null=True, blank=True)
     is_member = models.BooleanField('Membre', default=False)
+    is_resigned = models.BooleanField('Démission', default=False)
     is_staff = models.BooleanField('Animateur', default=False)
     is_committee = models.BooleanField('Comité', default=False)
     bank_name = models.CharField(max_length=200, default=None, null=True, blank=True)
     iban = models.CharField(max_length=200, default=None, null=True, blank=True)
     inscription_state = models.CharField(max_length=20, choices=INSCRIPTION_STATE, default='not member')
+    member_type = models.CharField(max_length=20, choices=MEMBER_TYPE, default=None, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name} {self.surname}'
@@ -35,6 +45,9 @@ class Project(models.Model):
     member = models.ForeignKey(Member, on_delete=models.PROTECT, default=None, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.member.visa}/{self.name}'
+        if self.member:
+            return f'{self.member.visa}/{self.name}'
+        else :
+            return f'{self.name}'
 
 
