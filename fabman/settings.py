e54d285a4af1dc67f1980578b1a0020bd23af4de
Alexandre Sierro <alexsierro@gmail.com>
2021-07-26 21:04:00 +0200

    Fix dumpdata encoding

--- fabman/settings.py
+++ fabman/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import _locale
+_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
