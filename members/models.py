e1780365129ef870aa5645e9eecdf58577b8195e
Alexandre Sierro <alexsierro@gmail.com>
2022-03-19 22:06:24 +0100

    Add member created and resigned date + remove inscription state

--- members/models.py
+++ members/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -28,13 +30,15 @@ class Member(models.Model):
     mail = models.EmailField('Email', max_length=200, default=None, null=True, blank=True)
     phone_number = models.CharField('Téléphone', max_length=25, default=None, null=True, blank=True)
     is_member = models.BooleanField('Membre', default=False)
+    member_type = models.CharField(max_length=20, choices=MEMBER_TYPE, default=None, null=True, blank=True)
+    date_added = models.DateField('Date ajout', default=date.today, null=True, blank=True)
     is_resigned = models.BooleanField('Démission', default=False)
+    date_resigned = models.DateField('Date démission', default=None, null=True, blank=True)
     is_staff = models.BooleanField('Animateur', default=False)
     is_committee = models.BooleanField('Comité', default=False)
     bank_name = models.CharField(max_length=200, default=None, null=True, blank=True)
     iban = models.CharField(max_length=200, default=None, null=True, blank=True)
-    inscription_state = models.CharField(max_length=20, choices=INSCRIPTION_STATE, default='not member')
-    member_type = models.CharField(max_length=20, choices=MEMBER_TYPE, default=None, null=True, blank=True)
+    #inscription_state = models.CharField(max_length=20, choices=INSCRIPTION_STATE, default='not member')
 
     def __str__(self):
         return f'{self.name} {self.surname}'
