448a9cc274f9fac5e0e1fa84dddd3c8cf9189aec
Alexandre Sierro <alexsierro@gmail.com>
2023-09-28 21:10:09 +0200

    Refactor invoice view

---
+++ invoicing/invoice_view_helpers.py
@@ -0,0 +1,88 @@
+import tempfile
+
+from django.db.models import Sum, Q
+from django.template.loader import get_template
+from qrbill import QRBill
+from stdnum.ch import esr
+from weasyprint import HTML
+
+from fabman.settings import MEDIA_ROOT, STATIC_ROOT, STATIC_URL, MEDIA_URL
+from invoicing.models import Invoice, Usage, AccountEntry
+
+
+def get_invoice_html(invoice_number, is_for_pdf=False):
+    invoice_number = str(invoice_number)
+    invoice = Invoice.objects.get(invoice_number=invoice_number)
+    number = invoice_number
+    number_ref = number + esr.calc_check_digit(number)
+
+    usages = Usage.objects.filter(invoice=invoice)
+    usages_annotated = usages.values('resource__name',
+                                     'comment',
+                                     'resource__unit__name',
+                                     'unit_price',
+                                     'project__name').annotate(qty=Sum('qty'), total_price=Sum('total_price')
+                                                               ).order_by('project__name')
+
+    # use_project is true if at last one project is used
+    projects = usages.values('project').distinct()
+    if projects.count() == 1 and projects.first()['project'] is None:
+        use_projects = False
+    else:
+        use_projects = True
+
+    amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(
+        total=Sum('total_price'))['total'] or 0
+
+    amount_other_usages = invoice.amount - amount_machine_usages
+
+    my_bill = QRBill(
+        language='fr',
+        ref_number=number_ref,
+        account='CH5530808007723788063',
+        creditor={
+            'name': 'FabLab Sion', 'pcode': '1950', 'city': 'Sion',
+        },
+        debtor={
+            'name': f'{invoice.member.name} {invoice.member.surname}', 'pcode': '1950', 'city': invoice.member.locality,
+            'street': invoice.member.address,
+        },
+        amount=invoice.amount_due,
+        top_line=True,
+        payment_line=True
+    )
+
+    with tempfile.TemporaryFile(encoding='utf-8', mode='r+') as temp:
+        my_bill.as_svg(temp)
+        temp.seek(0)
+        qrbill_svg = temp.read()
+
+    balance = AccountEntry.objects. \
+        filter(Q(date__lte=invoice.date_invoice) | Q(invoice=invoice), member=invoice.member, ). \
+        aggregate(machine=Sum('amount_machine'), cash=Sum('amount_cash'))
+
+    amount_cash_after = balance['cash'] or 0
+    amount_machine_after = balance['machine'] or 0
+
+    template = get_template('show_invoice.html')
+
+    html = template.render({'invoice': invoice,
+                            'member_info': invoice.member,
+                            'usages_anotated': usages_annotated,
+                            'amount_other_usages': amount_other_usages,
+                            'amount_machine_usages': amount_machine_usages,
+                            'amount_cash_after': amount_cash_after,
+                            'amount_machine_after': amount_machine_after,
+                            'use_projects': use_projects,
+                            'STATIC_PREFIX': ('file://' + STATIC_ROOT if is_for_pdf else STATIC_URL) + '/',
+                            'MEDIA_PREFIX': ('file://' + MEDIA_ROOT if is_for_pdf else MEDIA_URL) + '/',
+                            'QRBILL_SVG': qrbill_svg
+                            })
+
+    return html
+
+
+def get_invoice_pdf(invoice_number):
+    html = get_invoice_html(invoice_number, is_for_pdf=True)
+    pdf_file = HTML(string=html).write_pdf()
+    return pdf_file
--- invoicing/views.py
+++ invoicing/views.py
@@ -8,6 +8,7 @@ from django.template.loader import get_template
 from django.utils import timezone
 
 from fabman.settings import STATIC_ROOT, MEDIA_ROOT, STATIC_URL, MEDIA_URL
+from invoicing.invoice_view_helpers import get_invoice_pdf, get_invoice_html
 from invoicing.models import Invoice, Usage, AccountEntry
 from django.db.models import Max, Sum, Q
 from qrbill.bill import QRBill
@@ -119,86 +120,17 @@ def create(request):
         return redirect('show_invoice', invoice_number=result['invoice'].invoice_number)
 
 
-from weasyprint import HTML
-
-
 def show_invoice(request, invoice_number):
     if not request.user.is_staff:
         raise PermissionDenied
 
-    invoice = Invoice.objects.get(invoice_number=invoice_number)
-    number = invoice_number
-    number_ref = number + esr.calc_check_digit(number)
-
-    usages = Usage.objects.filter(invoice=invoice)
-    usages_annotated = usages.values('resource__name',
-                                     'comment',
-                                     'resource__unit__name',
-                                     'unit_price',
-                                     'project__name').annotate(qty=Sum('qty'), total_price=Sum('total_price')
-                                                               ).order_by('project__name')
-
-    # use_project is true if at last one project is used
-    projects = usages.values('project').distinct()
-    if projects.count() == 1 and projects.first()['project'] is None:
-        use_projects = False
-    else:
-        use_projects = True
-
-    amount_machine_usages = usages.filter(resource__payable_by_animation_hours=True).aggregate(
-        total=Sum('total_price'))['total'] or 0
-
-    amount_other_usages = invoice.amount - amount_machine_usages
-
-    my_bill = QRBill(
-        language='fr',
-        ref_number=number_ref,
-        account='CH5530808007723788063',
-        creditor={
-            'name': 'FabLab Sion', 'pcode': '1950', 'city': 'Sion',
-        },
-        debtor={
-            'name': f'{invoice.member.name} {invoice.member.surname}', 'pcode': '1950', 'city': invoice.member.locality,
-            'street': invoice.member.address,
-        },
-        amount=invoice.amount_due,
-        top_line=True,
-        payment_line=True
-    )
-
-    with tempfile.TemporaryFile(encoding='utf-8', mode='r+') as temp:
-        my_bill.as_svg(temp)
-        temp.seek(0)
-        qrbill_svg = temp.read()
-
-    balance = AccountEntry.objects. \
-        filter(Q(date__lte=invoice.date_invoice) | Q(invoice=invoice), member=invoice.member, ). \
-        aggregate(machine=Sum('amount_machine'), cash=Sum('amount_cash'))
-
-    amount_cash_after = balance['cash'] or 0
-    amount_machine_after = balance['machine'] or 0
-
     is_pdf = request.resolver_match.url_name == 'show_pdf_invoice'
 
-    template = get_template('show_invoice.html')
-
-    html = template.render({'invoice': invoice,
-                            'member_info': invoice.member,
-                            'usages_anotated': usages_annotated,
-                            'amount_other_usages': amount_other_usages,
-                            'amount_machine_usages': amount_machine_usages,
-                            'amount_cash_after': amount_cash_after,
-                            'amount_machine_after': amount_machine_after,
-                            'use_projects': use_projects,
-                            'STATIC_PREFIX': ('file://' + STATIC_ROOT if is_pdf else STATIC_URL) + '/',
-                            'MEDIA_PREFIX': ('file://' + MEDIA_ROOT if is_pdf else MEDIA_URL) + '/',
-                            'QRBILL_SVG': qrbill_svg
-                            })
-
     if is_pdf:
-        pdf_file = HTML(string=html).write_pdf()
+        pdf_file = get_invoice_pdf(invoice_number)
         response = HttpResponse(pdf_file, content_type="application/pdf")
     else:
+        html = get_invoice_html(invoice_number)
         response = HttpResponse(html)
 
     return response
