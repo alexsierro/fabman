86e23fbdb2a7a6071ca9d0c7b994c8da05913748
Alexandre Sierro <alexsierro@gmail.com>
2024-04-21 20:09:32 +0200

    Update mail for invoice reminder (subject and delay)

--- invoicing/invoice_mail_helper.py
+++ invoicing/invoice_mail_helper.py
@@ -5,6 +5,17 @@ from invoicing.invoice_view_helpers import get_invoice_pdf
 
 
 def send_invoice(invoice):
+
+    # set payment delay according to invoice status
+    if invoice.status == 'created':
+        payment_delay = 30
+    elif invoice.status == 'rappel1':
+        payment_delay = 10
+    elif invoice.status == 'rappel2':
+        payment_delay = 10
+    else:
+        payment_delay = 0
+
     body = \
 f"""Cher membre,
 
@@ -13,8 +24,8 @@ Voici la facture pour ton utilisation du FabLab Sion.
 Numéro de Facture : {invoice.invoice_number}
 Montant Total : {invoice.amount_due}    
 
-Nous te prions de bien vouloir régler cette facture dans un délai de 30 jours.
-
+Nous te prions de bien vouloir régler cette facture dans un délai de {payment_delay} jours.
+    
 Si tu as des questions ou des préoccupations concernant cette facture, n'hésite pas à me contacter en répondant à ce mail.
 
 Meilleures salutations,
@@ -26,8 +37,16 @@ Le caissier
 
     to = EMAIL_DEBUG_RECEIVER if DEBUG else invoice.member.mail
 
+    # add reminder level before subject
+    if invoice.status == 'rappel1':
+        pre_subject = 'Rappel - '
+    elif invoice.status == 'rappel2':
+        pre_subject = 'Rappel 2 - '
+    else:
+        pre_subject = ''
+
     email = EmailMessage(
-        f"Facture {invoice.invoice_number} - FabLab Sion",
+        f"{pre_subject}Facture {invoice.invoice_number} - FabLab Sion",
         body,
         EMAIL_FROM,
         [to],
