07177038ab3dc3c6ba4caaea0e28c3391cb41e03
Alexandre Sierro <alexsierro@gmail.com>
2026-02-24 13:37:20 +0100

    Update invoice_export_accounting to loop over different accounts

--- invoicing/invoices_export_accounting.py
+++ invoicing/invoices_export_accounting.py
@@ -9,18 +9,35 @@ def export_as_txt(self, request, queryset):
     response['Content-Disposition'] = 'attachment; filename={}.txt'.format('invoices')
     writer = csv.writer(response, delimiter='\t')
 
+    total = 0
+
+
+    accounts = {
+        'not use - interne': 'xxx',
+        'perte': 6710,
+        'bank': 1030,
+        'not use frais': 'xxx',
+        'cash': '1000',
+        'not use - deduction machine': 'z',
+        'not use - deduction cash': 'cf'
+    }
+
+
+
     writer.writerow(
         ['Date', 'Doc', 'Description', 'AccountDebit', 'AccountCredit', 'Amount'])
     for invoice in queryset.order_by('date_paid'):
-        if invoice.is_paid and invoice.payment_method == 'cash':
+        if invoice.is_paid:
             date_paid = invoice.date_paid
             doc = invoice.invoice_number
             description = f'Facture manager pour {invoice.member}'
-            account_debit = 1030
+            account_debit = accounts[invoice.payment_method]
             account_credit = 3633
             amount = invoice.amount_due
+            total += amount
 
             writer.writerow(
                 [date_paid, doc, description, account_debit, account_credit, amount])
 
+    print(f'Total: {total}')
     return response
